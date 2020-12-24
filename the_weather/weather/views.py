4c4889e46b62829af4a0f6d86a46a55d055ad112
sibiyan <sibiyan9030@gmail.com>
2020-12-25 01:26:21 +0530

    initial

---
+++ the_weather/weather/views.py
@@ -0,0 +1,37 @@
+import requests
+from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=7b4356841b51880d7adfe426ac3f0920'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        try:
+            r = requests.get(url.format(city)).json()
+
+            city_weather = {
+                'city': city.name,
+                'temperature': r['main']['temp'],
+                'description': r['weather'][0]['description'],
+                'icon': r['weather'][0]['icon'] + ".png",
+            }
+
+            weather_data.append(city_weather)
+        except:
+            pass
+
+    context = {'weather_data': weather_data, 'form': form}
+    return render(request, 'weather/weather.html', context)
