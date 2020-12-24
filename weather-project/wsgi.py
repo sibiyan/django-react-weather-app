2201bc8d0d5b572738a7ed0f07341c8d94d08485
sibiyan <sibiyan9030@gmail.com>
2020-12-25 01:58:17 +0530

    wsgi

--- weather-project/wsgi.py
+++ weather-project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "the_weather.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weather-project.settings")
 
 application = get_wsgi_application()
